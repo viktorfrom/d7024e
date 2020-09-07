e90e212157b98c8653a6db225f7de66932c35220
97gushan <97gustavh@gmail.com>
2020-09-07 08:46:26 +0000

    basic udp server

--- cmd/kademlia/kademlia.go
+++ cmd/kademlia/kademlia.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/viktorfrom/d7024e-kademlia/internal/network"
 )
 
 func main() {
 	fmt.Println("Hello, Arch!")
+
+	// setup UDP server on port 8080
+	go network.Listen("", "8080")
+	network.SendMessage(nil, nil)
 }
--- internal/network/network.go
+++ internal/network/network.go
@@ -1,18 +1,107 @@
 package network
 
 import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
 	. "github.com/viktorfrom/d7024e-kademlia/internal/kademlia"
 )
 
 type Network struct {
 }
 
-func Listen(ip string, port int) {
-	// TODO
+func Listen(ip string, port string) {
+
+	PORT := ":" + port
+	fmt.Println("start server")
+	s, err := net.ResolveUDPAddr("udp4", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("start listening")
+
+	connection, err := net.ListenUDP("udp4", s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer connection.Close()
+	buffer := make([]byte, 1024)
+	rand.Seed(time.Now().Unix())
+
+	for {
+		n, addr, err := connection.ReadFromUDP(buffer)
+		fmt.Print("-> ", string(buffer[0:n-1]))
+
+		if strings.TrimSpace(string(buffer[0:n])) == "STOP" {
+			fmt.Println("Exiting UDP server!")
+			return
+		}
+
+		data := []byte(strconv.Itoa(10))
+		fmt.Printf("data: %s\n", string(data))
+		_, err = connection.WriteToUDP(data, addr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
+// SendMessage Temporary UDP client sending example packages to an inputed IP address
+func SendMessage(contact *Contact, data []byte) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("enter IP to connect to >> ")
+	text, _ := reader.ReadString('\n')
+	CONNECT := strings.TrimSuffix(text, "\n") + ":8080"
+	fmt.Print(CONNECT)
+	s, err := net.ResolveUDPAddr("udp4", CONNECT)
+	c, err := net.DialUDP("udp4", nil, s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("The UDP server is %s\n", c.RemoteAddr().String())
+	defer c.Close()
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+		data := []byte(text + "\n")
+		_, err = c.Write(data)
+		if strings.TrimSpace(string(data)) == "STOP" {
+			fmt.Println("Exiting UDP client!")
+			return
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		buffer := make([]byte, 1024)
+		n, _, err := c.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Reply: %s\n", string(buffer[0:n]))
+	}
 }
 
 func (network *Network) SendPingMessage(contact *Contact) {
 	// TODO
+
 }
 
 func (network *Network) SendFindContactMessage(contact *Contact) {
