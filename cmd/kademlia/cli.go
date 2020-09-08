05dadf8635657ccddc42baf30ee3d3aeeb483002
Viktor From <viktor.from@grepit.se>
2020-09-08 10:19:52 +0200

    Update CLI

--- cmd/kademlia/cli.go
+++ cmd/kademlia/cli.go
@@ -1,14 +1,68 @@
-import "github.com/urfave/cli" 
+package main
 
-func cli() {
-app := &cli.App{
-			Name: "greet",
-			Usage: "say a greeting",
-			Action: func(c *cli.Context) error {
-				fmt.Println("Greetings")
-				return nil
-			},
-		}
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"github.com/urfave/cli"
+)
 
-app.Run(os.Args)
+var app = cli.NewApp()
+
+var str = []string{""}
+
+func info() {
+  app.Name = "Kademlia CLI"
+  app.Usage = "An example prototype CLI for Kademlia instructions"
+  app.Author = "viktorfrom, markhakansson, 97gushan" 
+  app.Version = "1.0.0"
+}
+
+func commands() {
+  app.Commands = []cli.Command{
+    {
+      Name:    "exit",
+      Aliases: []string{"e"},
+      Usage:   "Terminates specified node",
+      Action: func(c *cli.Context) { 
+        value := "node value" // TODO: retrieve actual value
+        content := append(str, value)
+        m := strings.Join(content, " ")
+        fmt.Println(m)
+      },
+    },
+    {
+      Name:    "get",
+      Aliases: []string{"g"},
+      Usage:   "Retrieves content of specified node",
+      Action: func(c *cli.Context) { 
+        value := "node value" // TODO: retrieve actual value
+        content := append(str, value)
+        m := strings.Join(content, " ")
+        fmt.Println(m)
+      },
+    },
+    {
+      Name:    "put",
+      Aliases: []string{"p"},
+      Usage:   "Appends node and content to network",
+      Action: func(c *cli.Context) { 
+        value := "node value" // TODO: retrieve actual value
+        content := append(str, value)
+        m := strings.Join(content, " ")
+        fmt.Println(m)
+      },
+    },
+  }
+}
+
+func main() {
+  info()
+  commands()
+
+  err := app.Run(os.Args)
+  if err != nil {
+    log.Fatal(err)
+  }
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/viktorfrom/d7024e-kademlia
 
 go 1.15
+
+require github.com/urfave/cli v1.22.4 // indirect
