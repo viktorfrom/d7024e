81cf0fe50b39327095840cfc0375f7329cd0f0d0
Viktor From <viktor.from91@gmail.com>
2020-09-14 21:25:23 +0200

    Update CLI

--- cmd/kademlia/cli.go
+++ cmd/kademlia/cli.go
@@ -1,137 +1,84 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
 	"os"
-	"time"
+	"strings"
 
-	"github.com/mvisonneau/gitlab-ci-pipelines-exporter/cmd"
 	"github.com/urfave/cli"
 )
 
-// Run handles the instanciation of the CLI application
-func main() {
-	Run("ads")
+var app = cli.NewApp()
 
+var str = []string{""}
+
+func info() {
+	app.Name = "Kademlia CLI"
+	app.Usage = "An example prototype CLI for Kademlia instructions"
+	app.Author = "viktorfrom, markhakansson, 97gushan"
+	app.Version = "1.0.0"
 }
 
-func Run(version string) {
-	NewApp(version, time.Now()).Run(os.Args)
-}
-
-// NewApp configures the CLI application
-func NewApp(version string, start time.Time) (app *cli.App) {
-	app = cli.NewApp()
-	app.Name = "gitlab-ci-pipelines-exporter"
-	app.Version = version
-	app.Usage = "Export metrics about GitLab CI pipelines statuses"
-	app.EnableBashCompletion = true
-
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:   "enable-pprof",
-			EnvVar: "GCPE_ENABLE_PPROF",
-			Usage:  "Enable profiling endpoints at /debug/pprof",
+func commands() {
+	app.Commands = []cli.Command{
+		{
+			Name:    "exit",
+			Aliases: []string{"e"},
+			Usage:   "Terminates specified node",
+			Action: func(c *cli.Context) {
+				value := "node value" // TODO: retrieve actual value
+				content := append(str, value)
+				m := strings.Join(content, " ")
+				fmt.Println(m)
+			},
 		},
-		cli.StringFlag{
-			Name:   "gitlab-token",
-			EnvVar: "GCPE_GITLAB_TOKEN",
-			Usage:  "GitLab access `token`. Can be use to override the gitlab token in config file",
+		{
+			Name:    "get",
+			Aliases: []string{"g"},
+			Usage:   "Retrieves content of specified node",
+			Action: func(c *cli.Context) {
+				value := "node value" // TODO: retrieve actual value
+				content := append(str, value)
+				m := strings.Join(content, " ")
+				fmt.Println(m)
+			},
 		},
-		cli.StringFlag{
-			Name:   "listen-address, l",
-			EnvVar: "GCPE_LISTEN_ADDRESS",
-			Usage:  "listen-address `address:port`",
-			Value:  ":8080",
-		},
-		cli.StringFlag{
-			Name:   "log-level",
-			EnvVar: "GCPE_LOG_LEVEL",
-			Usage:  "log `level` (debug,info,warn,fatal,panic)",
-			Value:  "info",
-		},
-		cli.StringFlag{
-			Name:   "log-format",
-			EnvVar: "GCPE_LOG_FORMAT",
-			Usage:  "log `format` (json,text)",
-			Value:  "text",
+		{
+			Name:    "put",
+			Aliases: []string{"p"},
+			Usage:   "Appends node and content to network",
+			Action: func(c *cli.Context) {
+				value := "node value" // TODO: retrieve actual value
+				content := append(str, value)
+				m := strings.Join(content, " ")
+				fmt.Println(m)
+			},
 		},
 	}
-
-	app.Action = cmd.ExecWrapper(cmd.Run)
-
-	app.Metadata = map[string]interface{}{
-		"startTime": start,
-	}
-
-	return
 }
 
+func StartCli() {
+	reader := bufio.NewReader(os.Stdin)
 
-// import (
-// 	"fmt"
-// 	"log"
-// 	"os"
-// 	"strings"
+	for {
+		input, _ := reader.ReadString('\n')
+		if input == "\n" || input == "" {
+			// fmt.Print(">> ")
+			continue
+		} else {
+			// str = append(str, input)
+			fmt.Print("str = ", str)
 
-// 	"github.com/urfave/cli"
-// )
+			info()
+			commands()
 
-// var app = cli.NewApp()
+			err := app.Run(os.Args)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 
-// var str = []string{""}
-
-// func info() {
-// 	app.Name = "Kademlia CLI"
-// 	app.Usage = "An example prototype CLI for Kademlia instructions"
-// 	app.Author = "viktorfrom, markhakansson, 97gushan"
-// 	app.Version = "1.0.0"
-// }
-
-// func commands() {
-// 	app.Commands = []cli.Command{
-// 		{
-// 			Name:    "exit",
-// 			Aliases: []string{"e"},
-// 			Usage:   "Terminates specified node",
-// 			Action: func(c *cli.Context) {
-// 				value := "node value" // TODO: retrieve actual value
-// 				content := append(str, value)
-// 				m := strings.Join(content, " ")
-// 				fmt.Println(m)
-// 			},
-// 		},
-// 		{
-// 			Name:    "get",
-// 			Aliases: []string{"g"},
-// 			Usage:   "Retrieves content of specified node",
-// 			Action: func(c *cli.Context) {
-// 				value := "node value" // TODO: retrieve actual value
-// 				content := append(str, value)
-// 				m := strings.Join(content, " ")
-// 				fmt.Println(m)
-// 			},
-// 		},
-// 		{
-// 			Name:    "put",
-// 			Aliases: []string{"p"},
-// 			Usage:   "Appends node and content to network",
-// 			Action: func(c *cli.Context) {
-// 				value := "node value" // TODO: retrieve actual value
-// 				content := append(str, value)
-// 				m := strings.Join(content, " ")
-// 				fmt.Println(m)
-// 			},
-// 		},
-// 	}
-// }
-
-// // StartCli TODO
-// func StartCli() {
-// 	info()
-// 	commands()
-
-// 	err := app.Run(os.Args)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
+	}
+}
