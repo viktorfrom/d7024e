26817c9aad4a11e77d0e840d1050516a9b882009
Mark Hakansson <mark.hakansson@live.se>
2020-09-14 15:48:54 +0200

    update randarr with random hex strings functionality

--- pkg/randarr/randarr.go
+++ pkg/randarr/randarr.go
@@ -1,8 +1,15 @@
 package randarr
 
 import (
+	"encoding/hex"
+	"errors"
+	"fmt"
 	"math/rand"
 	"time"
+)
+
+const (
+	errUneven string = "n is not an even number"
 )
 
 // RandomBytes returns a byte slice with n number of random bytes
@@ -11,4 +18,17 @@ func RandomBytes(n int) []byte {
 	arr := make([]byte, n)
 	rand.Read(arr)
 	return arr
+}
+
+// RandomHexString returns a hexadecimal string with the size n bytes
+// n has to be an even number
+func RandomHexString(n int) (string, error) {
+	if n%2 != 0 {
+		return "", errors.New(errUneven)
+	}
+	n = n / 2
+	arr := RandomBytes(n)
+	fmt.Println("arr: ", arr)
+	encodedString := hex.EncodeToString(arr)
+	return encodedString, nil
 }
--- pkg/randarr/randarr_test.go
+++ pkg/randarr/randarr_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCorrectLength(t *testing.T) {
+func TestByteArrayCorrectLength(t *testing.T) {
 	zero := RandomBytes(0)
 	ten := RandomBytes(10)
 	twenty := RandomBytes(20)
@@ -16,4 +16,21 @@ func TestCorrectLength(t *testing.T) {
 	assert.Equal(t, 10, len(ten))
 	assert.Equal(t, 20, len(twenty))
 	assert.Equal(t, 30, len(thirty))
+}
+
+func TestHexStrCorrectLength(t *testing.T) {
+	zero, _ := RandomHexString(0)
+	ten, _ := RandomHexString(10)
+	twenty, _ := RandomHexString(20)
+	thirty, _ := RandomHexString(30)
+
+	assert.Equal(t, 0, len(zero))
+	assert.Equal(t, 10, len([]byte(ten)))
+	assert.Equal(t, 20, len([]byte(twenty)))
+	assert.Equal(t, 30, len([]byte(thirty)))
+}
+
+func TestHexStrOddNumber(t *testing.T) {
+	_, err := RandomHexString(3)
+	assert.Error(t, err)
 }
