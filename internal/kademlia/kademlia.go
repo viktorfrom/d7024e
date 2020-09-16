3d28dc7a45f6c9018c03b3848cb97e46366a6056
97gushan <97gustavh@gmail.com>
2020-09-16 12:03:45 +0000

    remove prints

--- internal/kademlia/kademlia.go
+++ internal/kademlia/kademlia.go
@@ -2,7 +2,6 @@ package kademlia
 
 import (
 	"fmt"
-	"strconv"
 )
 
 type Kademlia struct {
@@ -41,15 +40,10 @@ func (kademlia *Kademlia) Ping() {
 	target := &kademlia.RT.FindClosestContacts(kademlia.RT.me.ID, bucketSize)[0]
 	rpc, err := kademlia.network.SendPingMessage(target, &kademlia.RT.me)
 
-	fmt.Println("Send ping")
-
 	if err != nil {
 		fmt.Println(err)
 		kademlia.RT.RemoveContact(*target)
 	} else if *rpc.Type == "OK" {
-		fmt.Println("Add " + target.ID.String() + " to bucket id: " + strconv.Itoa(kademlia.RT.getBucketIndex(target.ID)))
 		kademlia.RT.AddContact(*target)
-	} else {
-		fmt.Print("CAOS?")
 	}
 }
