fb7140eca3c51dbcf2dec91f962328976840d921
Viktor From <viktor.from91@gmail.com>
2020-10-03 13:27:03 +0200

    Add nodelookup test

--- internal/kademlia/node_test.go
+++ internal/kademlia/node_test.go
@@ -6,6 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNodeLookup(t *testing.T) {
+	node := Node{}
+	node.InitNode()
+
+	c := NewContact(NewNodeID("00000000000000000000000000000000FFFFFFFF"), "10.0.8.3:8080")
+	c.CalcDistance(node.RT.GetMeID())
+	y := node.NodeLookup(c.ID)
+	assert.Equal(t, 0, len(y))
+
+}
+
+func TestSearchLocalStore(t *testing.T) {
+	node := Node{nil, Network{nil, ""}, make(map[string]string)}
+	node.insertLocalStore("hello", "there")
+
+	c := node.searchLocalStore("hello")
+	assert.Equal(t, "there", *c)
+
+	y := node.searchLocalStore("shouldNotExist")
+	assert.Nil(t, y)
+
+}
+
 func TestGenerateRefreshNodeValue(t *testing.T) {
 	assert.Equal(t, "0000000000000000000000000000000000000002", generateRefreshNodeValue(1, 1).String())
 	assert.Equal(t, "0000000000000000000000000000000000000004", generateRefreshNodeValue(2, 1).String())
